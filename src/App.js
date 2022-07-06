05325cece3489457445ae0d7d4d9d9c1d66e106d
yasin yağmur <yasin.yagmur@gmail.com>
2022-07-06 18:24:36 +0300

    fixed

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import AppRouter from './Router/AppRouter';
+
+function App() {
+  return (
+    <div className="container">
+      <AppRouter/>
+    </div>
+  );
+}
+
+export default App;
